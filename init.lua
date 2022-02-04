12180a5c4d99f556ca8fcfe484fc4937c30cd6b0
Vov83654518 <92204331+Vov83654518@users.noreply.github.com>
2022-02-04 12:44:16 +0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -21,9 +21,15 @@ temp = temperatur.read()
 idial = 80
 dop = 5
 
+if(temp <= idial-dop)then 
+    gpio.write(4, gpio.HIGH);
+elseif(temp > idial-dop)then
+    gpio.write(4, gpio.LOW);
+end
 
 t=0
 tmr.alarm(0,1000, 1, function() t=t+1 if t>999 then t=0 end end)
+
 srv=net.createServer(net.TCP, 1000)
 srv:listen(80,function(conn)
     conn:on("receive",function(client,request)
@@ -43,11 +49,6 @@ srv:listen(80,function(conn)
         temperatur.setup(gpio0)
         addrs = temperatur.addrs()
         temp = temperatur.read()
-        if(temp <= idial-dop)then 
-            gpio.write(4, gpio.HIGH);
-        elseif(temp > idial-dop)then
-            gpio.write(4, gpio.LOW);
-      end
     elseif(_GET.pin == "SIP")then
         temperatur.setup(gpio0)
         addrs = temperatur.addrs()
@@ -77,17 +78,19 @@ srv:listen(80,function(conn)
             dop = dop - 5
         end
     end
+
+    tempn = temp - temp%1
     conn:send('HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nCache-Control: private, no-store\r\n\r\n\
    <!DOCTYPE HTML>\
    <html><body bgcolor="#6699ff"><h1>&nbsp</h1>\
-<h1>Температура             :&nbsp'..temp..'</h1>\
-<h1>Желаемая температура    :&nbsp'..idial..'</h1>\
-<a href=\"?pin=SIM\"><button style=\"background: #ff3300; color: White; border-radius: 150px; font-size: 40px;\">.   -5    .</button></a>&nbsp;<a href=\"?pin=SIP\">\
-<button style=\"background: #B0CE3A; color: White; border-radius: 150px; font-size: 40px;\">.   +5    .</button></a>\
-<h1>Просадка                :&nbsp'..dop..'</h1>\
-<a href=\"?pin=SDM\"><button style=\"background: #ff3300; color: White; border-radius: 150px; font-size: 40px;\">.&nbsp-5&nbsp.</button></a>&nbsp;<a href=\"?pin=SDP\">\
-<button style=\"background: #B0CE3A; color: White; border-radius: 150px; font-size: 40px;\">.&nbsp+5&nbsp.</button></a>\
-<h1><a href=\"?pin=ON1\"><button style=\"background: #B0CE3A; color: White; border-radius: 180px; font-size: 70px;\">Обновить</button></a></h1>\
+<h1 align="center"><big><big>Температура                :&nbsp'..tempn..'</big></big></h1>\
+<h1 align="center"><big><big>Нагрев до                  :&nbsp'..idial..'</big></big></h1>\
+<h1 align="center"><a href=\"?pin=SIM\"><button style=\"background: #ff3300; color: White; border-radius: 50px;  height:70px;  font-size: 50px;  width:200px;\">-5</button></a>&nbsp;<a href=\"?pin=SIP\">\
+<button style=\"background: #B0CE3A; color: White; border-radius: 50px;  height:70px;  font-size: 50px;  width:200px;\"> +5 </button></a></h1>\
+<h1 align="center"><big><big>Просадка                   :&nbsp'..dop..'</big></big></h1>\
+<h1 align="center"><a href=\"?pin=SDM\"><button style=\"background: #ff3300; color: White; border-radius: 50px;  height:70px;  font-size: 50px;  width:200px;\">-5</button></a>&nbsp;<a href=\"?pin=SDP\">\
+<button style=\"background: #B0CE3A; color: White; border-radius: 50px;  height:70px;  font-size: 50px;  width:200px;\">+5</button></a></h1>\
+<h1 align="center"><a href=\"?pin=ON1\"><button style=\"background: #B0CE3A; color: White; border-radius: 180px; font-size: 70px;\">Обновить</button></a></h1>\
 </body></html>') 
     conn:on("sent",function(conn) conn:close() end)
     collectgarbage();
@@ -96,7 +99,7 @@ end)
 
 while true do
 
-tmr.alarm(0, 100, 1, function()
+tmr.alarm(0, 10000, 1, function()
     tmr.alarm(0, 100, 1, function()
         temperatur = require("ds18b20")
         gpio0 = 3
@@ -119,4 +122,5 @@ tmr.alarm(0, 100, 1, function()
       end
 
 end)
+
 end
